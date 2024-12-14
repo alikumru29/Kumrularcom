1223fd0522e5bb533a871ca94bc77a93e5b42e49
alikumru29 <130920504+alikumru29@users.noreply.github.com>
2024-12-14 19:56:30 +0300

    v1

--- src/server/middleware/security.ts
+++
@@ -1,6 +0,0 @@
-import { ExpressMiddleware } from "../types/express.js";
-
-export const securityMiddleware: ExpressMiddleware = (_req, res, next) => {
-  res.setHeader("X-Content-Type-Options", "nosniff");
-  next();
-};
