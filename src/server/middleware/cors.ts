2687b11b814e7a22945802d4492fddc564bbd655
alikumru29 <130920504+alikumru29@users.noreply.github.com>
2024-12-14 14:27:24 +0300

    v1

--- src/server/middleware/cors.ts
+++ src/server/middleware/cors.ts
@@ -2,6 +2,10 @@ import cors from "cors";
 import { env } from "../config/env.js";
 
 export const corsMiddleware = cors({
-  origin: env.isProduction ? "https://kumrular.com" : true,
+  origin: env.isProduction
+    ? ["https://kumrular.com", "https://www.kumrular.com"]
+    : true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { getProducts } from "../controllers/productController.js";
+import { HEADERS } from "../config/constants.js";
 
 const router = Router();
+
+// API middleware to set proper headers
+router.use((_req, res, next) => {
+  res.setHeader(HEADERS.CONTENT_TYPE, "application/json; charset=utf-8");
+  res.setHeader("X-Powered-By", "Kumrular API");
+  next();
+});
 
 router.get("/products", getProducts);
 
